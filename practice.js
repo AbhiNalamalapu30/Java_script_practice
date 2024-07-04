bb23dcd8e765fded0d780cdd58cd255fb4821b39
Narasimha-Nalamalapu <Nnreddy.us@gmail.com>
2024-07-04 13:02:38 -0400

    javascript_practice

---
+++ practice.js
@@ -0,0 +1,47 @@
+// let val1 = 21
+// let val2  = 89
+
+// if(val1 > 60 && val2 < 30 ){
+//     console.log('is greater than ')
+// }
+// else{
+//     console.log('is not greater than')
+// }
+
+let greeting = 'Hello'
+let name  = 'Abhi'
+let welcomemessage = greeting + name
+console.log(welcomemessage)
+
+let m = 10;
+let n = 5;
+console.log((m > 5 && n < 10) || (m < 5 && n > 10));
+
+console.log("Good" + " " + "Morning");
+
+let k = 5;
+let l = "5";
+console.log(k == l);
+
+let i = 7;
+let j = 30;
+console.log(i > 10 || j < 35);
+
+let g = 12;
+let h = 25;
+console.log(g < 15 && h > 20);
+
+let str1 = "Hello";
+let str2 = "World";
+console.log(str1 + " " + str2);
+
+let val1 = 21
+let val2  = 89
+
+if(val1 > 60 && val2 < 30 ){
+    console.log('is greater than ')
+}
+else{
+    console.log('is not greater than')
+}
+
