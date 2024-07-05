7d54ac7a2693da091ab15f27131545062085731f
Narasimha-Nalamalapu <Nnreddy.us@gmail.com>
2024-07-05 12:41:46 -0400

    javascript_practice

--- else if.js
+++ else if.js
@@ -1,3 +1,13 @@
+// let a = 10
+// let b = 30 + a
+// console.log(b)
+
 let a = 10
-let b = 30 + a
-console.log(b)
+let b = 20
+
+if(a>b){
+    console.log('it is greater than')
+}
+else {
+    console.log("it is not greater than")
+}
